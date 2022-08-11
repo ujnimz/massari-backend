87056688e111ffaddf36d5e447090f15a52e6cb5
Ujith Nimantha <ujithnimantha@gmail.com>
2022-08-11 08:42:49 +0300

    Cors disabled

--- app.js
+++ app.js
@@ -14,12 +14,12 @@ if (process.env.NODE_ENV !== 'PRODUCTION') {
 }
 
 // Adding Cors
-const corsOptions = {
-  origin: 'http://localhost:3000',
-  credentials: true, //access-control-allow-credentials:true
-  optionSuccessStatus: 200,
-};
-app.use(cors(corsOptions));
+// const corsOptions = {
+//   origin: 'http://localhost:3000',
+//   credentials: true, //access-control-allow-credentials:true
+//   optionSuccessStatus: 200,
+// };
+// app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(cookieParser());
