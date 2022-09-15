d811edd24f654bff202d4e355f25fc91353aaec6
Ujith Nimantha <ujithnimantha@gmail.com>
2022-09-15 16:53:10 +0300

    Minor update on Product Model

--- controllers/productController.js
+++ controllers/productController.js
@@ -8,26 +8,25 @@ const cloudinary = require('cloudinary');
 exports.createProduct = catchAsyncErrors(async (req, res, next) => {
   let images = [];
 
-  if (typeof req.body.images === 'string') {
-    images.push(req.body.images);
-  } else {
+  if (req.body.images.length > 0) {
     images = req.body.images;
+
+    const imagesLinks = [];
+
+    for (let i = 0; i < images.length; i++) {
+      const result = await cloudinary.v2.uploader.upload(images[i], {
+        folder: 'products',
+      });
+
+      imagesLinks.push({
+        public_id: result.public_id,
+        url: result.secure_url,
+      });
+    }
+
+    req.body.images = imagesLinks;
   }
 
-  const imagesLinks = [];
-
-  for (let i = 0; i < images.length; i++) {
-    const result = await cloudinary.v2.uploader.upload(images[i], {
-      folder: 'products',
-    });
-
-    imagesLinks.push({
-      public_id: result.public_id,
-      url: result.secure_url,
-    });
-  }
-
-  req.body.images = imagesLinks;
   req.body.user = req.user.id;
 
   const product = await Product.create(req.body);
